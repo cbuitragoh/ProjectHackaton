04c97642c732dc29a2ab1d40f44d836c5614c8d1
CarlosB <bhca0629@yahoo.es>
2021-12-14 15:35:21 -0500

    validations in register talentoso and gestor

--- FrontEnd/register/register.mjs
+++ FrontEnd/register/register.mjs
@@ -1,8 +1,38 @@
 import { post } from '../javascript/CRUD.mjs';
+import { validateFormGestor, checkName, checkCompany, checkEmail, checkPhone, checkContrasena, checkTags} from './register-gestor/formGestor.mjs';
+import { checkTalent } from './register-talentoso/formTalentoso.mjs';
 
 const registerForm = document.getElementById('register-form');
-const registerFail = document.getElementsByClassName('register-fail')
-const registerSuccess = document.getElementsByClassName('register-success')
+const registerFail = document.getElementsByClassName('register-fail');
+const registerSuccess = document.getElementsByClassName('register-success');
+
+registerForm[0].addEventListener('input', () => {
+    checkName(registerForm[0].value);
+})
+
+registerForm[1].addEventListener('input', () => {
+    checkEmail(registerForm[1].value);
+})
+
+registerForm[2].addEventListener('input', () => {
+    checkPhone(registerForm[2].value);
+})
+
+registerForm[3].addEventListener('input', () => {
+    if(registerForm.id=='company'){
+        checkCompany(registerForm[3].value);
+    }else{
+        checkTalent(registerForm[3].value);
+    }
+})
+
+registerForm[4].addEventListener('input', () => {
+    checkContrasena(registerForm[4].value);
+})
+
+registerForm[5].addEventListener('input', () => {
+    checkTags(registerForm[5].value);
+})
 
 registerForm.addEventListener('submit', async ($event) => {
     $event.preventDefault();
@@ -10,7 +40,7 @@ registerForm.addEventListener('submit', async ($event) => {
             if (data.status === 200) {
                 registerSuccess[0].style.display = 'block';
 
-                window.location.href = "/login";
+                window.location.href = "/";
             } else {
                 registerFail[0].style.display = 'block';
                 setTimeout(() => {
