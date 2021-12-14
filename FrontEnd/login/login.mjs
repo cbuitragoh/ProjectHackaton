545ced49e333355013f15f9244f15583acd76bac
CarlosB <bhca0629@yahoo.es>
2021-12-13 23:30:17 -0500

    validate login

--- FrontEnd/login/login.mjs
+++ FrontEnd/login/login.mjs
@@ -1,12 +1,27 @@
+import { checkEmail, checkContrasena, validateForm } from './form.mjs';
 import { post } from '../javascript/CRUD.mjs';
 
 const loginForm = document.getElementById('form-login');
 const loginFail = document.getElementsByClassName('login-fail')
 const loginSuccess = document.getElementsByClassName('login-success')
+const userEmail = document.getElementById('email');
+const userContrasena = document.getElementById('contrasena');
+
+
+userEmail.addEventListener('input', () => {
+    checkEmail(userEmail.value);
+})
+
+userContrasena.addEventListener('input', () => {
+    checkContrasena(userContrasena.value);
+})
+
+
 
 loginForm.addEventListener('submit', async ($event) => {
     $event.preventDefault();
-    await login().then((data) => {
+    if(validateForm()){
+        await login().then((data) => {
             if (data.status === 200) {
                 loginSuccess[0].style.display = 'block'
                 data.json().then((user) => {
@@ -18,12 +33,15 @@ loginForm.addEventListener('submit', async ($event) => {
             }
         });
         
+    }
+    
 })
 
 async function login() {
     const formElements = document.getElementById('form-login').elements;
     const url = '/API/login';
     
+
     const credentials = {
          email: formElements[0].value ,
          password: formElements[1].value,
@@ -32,3 +50,4 @@ async function login() {
     return await post(url, credentials);
  
 }
+
